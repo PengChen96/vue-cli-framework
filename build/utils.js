5ad6c550a8cb436e4e1d164595cea4023667c75f
pengchen <2601349541@qq.com>
2018-01-26 16:43:54 +0800

    打包配置 1）index.html 没有引号 2）fonts的路径问题

--- build/utils.js
+++ build/utils.js
@@ -47,6 +47,7 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
+        publicPath: '../../',   // fonts 路径不对问题
         fallback: 'vue-style-loader'
       })
     } else {
