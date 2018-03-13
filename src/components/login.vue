b596eb369539b8670cab455b0d7f869e5bab2033
pengchen <2601349541@qq.com>
2018-03-13 18:45:31 +0800

    save

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,19 +1,24 @@
 <template>
-    <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
+  <div class="container">
+    <div class="box">
+      <h1>XX管理系统</h1>
+      <Form ref="formInline" :model="formInline" :rules="ruleInline">
         <FormItem prop="user">
-            <Input type="text" v-model="formInline.user" placeholder="Username">
-            <Icon type="ios-person-outline" slot="prepend"></Icon>
-            </Input>
+          <Input type="text" v-model="formInline.user" placeholder="账号">
+          <Icon type="ios-person-outline" slot="prepend"></Icon>
+          </Input>
         </FormItem>
         <FormItem prop="password">
-            <Input type="password" v-model="formInline.password" placeholder="Password">
-            <Icon type="ios-locked-outline" slot="prepend"></Icon>
-            </Input>
+          <Input type="password" v-model="formInline.password" placeholder="密码">
+          <Icon type="ios-locked-outline" slot="prepend"></Icon>
+          </Input>
         </FormItem>
         <FormItem>
-            <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+          <Button type="primary" @click="handleSubmit('formInline')" long>登录</Button>
         </FormItem>
-    </Form>
+      </Form>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
 import { getPermissionList } from '@/api/permissionApi.js'
@@ -26,7 +31,7 @@ export default {
       },
       ruleInline: {
         user: [
-          { required: true, message: '请输入用户名.', trigger: 'blur' }
+          { required: true, message: '请输入账号.', trigger: 'blur' }
         ],
         password: [
           { required: true, message: '请输入密码.', trigger: 'blur' }
@@ -62,3 +67,28 @@ export default {
   }
 }
 </script>
+<style lang="less">
+  .container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #eee;
+    .box{
+      h1{
+        margin-bottom: 12px;
+        text-align: center;
+      }
+      width:360px;
+      /*height:230px;*/
+      background: #fff;
+      margin-top: -160px;
+      padding: 12px 24px;
+      border:1px solid #eee;
+      border-radius: 4px;
+      box-shadow: 4px 6px 10px 1px #bbb;
+    }
+  }
+</style>
